f2a8abcb933d4f384ee1ac5ae54c251791973f75
lottenOhpen <lotten@ohpen.com>
2021-06-24 13:48:41 +0200

    Add policy for VPCAccessExecution to execution role when lambda is running within VPC

--- locals.tf
+++ locals.tf
@@ -1,11 +1,11 @@
 locals {
-    s3_bucket               = var.s3_existing_package != null ? lookup(var.s3_existing_package, "bucket", null) : null
-    s3_key                  = var.s3_existing_package != null ? lookup(var.s3_existing_package, "key", null) : null
-    #s3_object_version      = var.s3_existing_package != null ? lookup(var.s3_existing_package, "version_id", null) : (var.store_on_s3 ? element(concat(aws_s3_bucket_object.lambda_package.*.version_id, [null]), 0) : null)
-    create_role             = var.create_function && var.create_role
-    role_name               = local.create_role ? coalesce(var.role_name, var.function_name, "*") : null
-    account_id              = data.aws_caller_identity.current.account_id
-    filename                = var.local_existing_package != null ? var.local_existing_package : null 
-    cloudwatch_policy_arn   = local.create_role ? join("", data.aws_iam_policy_document.logs.*.json) : null
-    s3_policy_arn           = var.s3_existing_package != null && local.create_role ? join("", data.aws_iam_policy_document.s3.*.json) : null
+  s3_bucket = var.s3_existing_package != null ? lookup(var.s3_existing_package, "bucket", null) : null
+  s3_key    = var.s3_existing_package != null ? lookup(var.s3_existing_package, "key", null) : null
+  #s3_object_version      = var.s3_existing_package != null ? lookup(var.s3_existing_package, "version_id", null) : (var.store_on_s3 ? element(concat(aws_s3_bucket_object.lambda_package.*.version_id, [null]), 0) : null)
+  create_role           = var.create_function && var.create_role
+  role_name             = local.create_role ? coalesce(var.role_name, var.function_name, "*") : null
+  account_id            = data.aws_caller_identity.current.account_id
+  filename              = var.local_existing_package != null ? var.local_existing_package : null
+  cloudwatch_policy_arn = local.create_role ? join("", data.aws_iam_policy_document.logs.*.json) : null
+  s3_policy_arn         = var.s3_existing_package != null && local.create_role ? join("", data.aws_iam_policy_document.s3.*.json) : null
 }
--- main.tf
+++ main.tf
@@ -1,79 +1,92 @@
 resource "aws_lambda_function" "default" {
-  count                           = var.create_function ? 1 : 0
-  function_name                   = var.function_name
-  role                            = var.create_role ? aws_iam_role.lambda[0].arn : var.role_name
-  handler                         = var.handler
-  runtime                         = var.runtime
-  kms_key_arn                     = var.kms_key_arn
-  timeout                         = var.timeout
-  s3_bucket                       = local.s3_bucket
-  s3_key                          = local.s3_key
-  filename                        = local.filename 
-  memory_size                     = var.memory_size
-  reserved_concurrent_executions  = var.reserved_concurrent_executions
-  tags                            = length(var.lambda_tags) > 0 ? merge(var.tags, var.lambda_tags) : var.tags
+  count                          = var.create_function ? 1 : 0
+  function_name                  = var.function_name
+  role                           = var.create_role ? aws_iam_role.lambda[0].arn : var.role_name
+  handler                        = var.handler
+  runtime                        = var.runtime
+  kms_key_arn                    = var.kms_key_arn
+  timeout                        = var.timeout
+  s3_bucket                      = local.s3_bucket
+  s3_key                         = local.s3_key
+  filename                       = local.filename
+  memory_size                    = var.memory_size
+  reserved_concurrent_executions = var.reserved_concurrent_executions
+  tags                           = length(var.lambda_tags) > 0 ? merge(var.tags, var.lambda_tags) : var.tags
 
   dynamic "vpc_config" {
-    for_each                      = var.vpc_subnet_ids != null && var.vpc_security_group_ids != null ? [true] : []
+    for_each = var.vpc_subnet_ids != null && var.vpc_security_group_ids != null ? [true] : []
     content {
-      security_group_ids          = var.vpc_security_group_ids
-      subnet_ids                  = var.vpc_subnet_ids
+      security_group_ids = var.vpc_security_group_ids
+      subnet_ids         = var.vpc_subnet_ids
     }
   }
 
   dynamic "tracing_config" {
-    for_each                      = var.tracing_mode == null ? [] : [true]
+    for_each = var.tracing_mode == null ? [] : [true]
     content {
-            mode                  = var.tracing_mode
+      mode = var.tracing_mode
     }
   }
 
   dynamic "environment" {
-    for_each                      = length(keys(var.environment_variables)) == 0 ? [] : [true]
+    for_each = length(keys(var.environment_variables)) == 0 ? [] : [true]
     content {
-            variables             = var.environment_variables
+      variables = var.environment_variables
     }
   }
 }
 
 resource "aws_cloudwatch_log_group" "log" {
-  count                           = var.create_function && var.create_cloudwatch_log_group ? 1 : 0
-  name                            = "/aws/lambda/${var.function_name}"
-  retention_in_days               = var.cloudwatch_logs_retention_in_days
-  kms_key_id                      = var.cloudwatch_logs_kms_key_id
-  tags                            = length(var.cloudwatch_tags) > 0 ? merge(var.tags, var.cloudwatch_tags) : var.tags
- }
+  count             = var.create_function && var.create_cloudwatch_log_group ? 1 : 0
+  name              = "/aws/lambda/${var.function_name}"
+  retention_in_days = var.cloudwatch_logs_retention_in_days
+  kms_key_id        = var.cloudwatch_logs_kms_key_id
+  tags              = length(var.cloudwatch_tags) > 0 ? merge(var.tags, var.cloudwatch_tags) : var.tags
+}
 
 resource "aws_iam_role" "lambda" {
-  count                           = local.create_role ? 1 : 0
-  name                            = local.role_name
-  assume_role_policy              = data.aws_iam_policy_document.assume_role[0].json
-  tags                            = length(var.role_tags) > 0 ? merge(var.tags, var.role_tags) : var.tags
+  count              = local.create_role ? 1 : 0
+  name               = local.role_name
+  assume_role_policy = data.aws_iam_policy_document.assume_role[0].json
+  tags               = length(var.role_tags) > 0 ? merge(var.tags, var.role_tags) : var.tags
 }
 
 resource "aws_iam_policy" "logs" {
-  count      = local.create_role ? 1 : 0
-  name       = "${var.function_name}-logs-policy"
-  policy     = local.cloudwatch_policy_arn
+  count  = local.create_role ? 1 : 0
+  name   = "${var.function_name}-logs-policy"
+  policy = local.cloudwatch_policy_arn
+}
+
+resource "aws_iam_policy" "vpcAccessExecution" {
+  count  = local.create_role && length(coalesce(var.vpc_subnet_ids, [])) >= 1 ? 1 : 0
+  name   = "${var.function_name}-vpcAccessExecution-policy"
+  policy = join("", data.aws_iam_policy_document.vpcAccessExecution.*.json)
 }
 
 resource "aws_iam_policy" "s3" {
-  count      = local.s3_policy_arn != null ? 1 : 0 
-  name       = "${var.function_name}-s3-policy"
-  policy     = local.s3_policy_arn
+  count  = local.s3_policy_arn != null ? 1 : 0
+  name   = "${var.function_name}-s3-policy"
+  policy = local.s3_policy_arn
+}
+
+resource "aws_iam_role_policy_attachment" "vpcAccessExecution" {
+  count      = local.create_role && length(coalesce(var.vpc_subnet_ids, [])) >= 1 ? 1 : 0
+  role       = aws_iam_role.lambda[0].name
+  policy_arn = aws_iam_policy.vpcAccessExecution[0].arn
+
 }
 
 resource "aws_iam_role_policy_attachment" "logs" {
-  count      = local.create_role ? 1 : 0 #local.cloudwatch_policy_arn != null ? 1 : 0
+  count = local.create_role ? 1 : 0 #local.cloudwatch_policy_arn != null ? 1 : 0
   #name       = "${local.role_name}-logs"
   role       = aws_iam_role.lambda[0].name
   policy_arn = aws_iam_policy.logs[0].arn
-  
+
 }
 
 resource "aws_iam_role_policy_attachment" "s3" {
-  count       = local.s3_policy_arn != null ? 1 : 0 
+  count = local.s3_policy_arn != null ? 1 : 0
   #name       = "${local.role_name}-s3"
-  role        = aws_iam_role.lambda[0].name
-  policy_arn  = aws_iam_policy.s3[0].arn
+  role       = aws_iam_role.lambda[0].name
+  policy_arn = aws_iam_policy.s3[0].arn
 }
